56f86486994f918b8592fe42517085a530361cc5
thomazottoni <thomaz.ottoni@gmail.com>
2017-09-15 06:59:43 -0300

    Add files via upload

--- www/js/loader.js
+++ www/js/loader.js
@@ -12,7 +12,7 @@ function executeCode(pTag, loadDelay) {
   // In order to prevent freezing the device (because too much work),
   // waiting kind of times is better when you use the multiple maps in one page.
   setTimeout(function() {
-    eval(code.innerText);
+    eval("var div = document.getElementById('map_canvas1'); var map = plugin.google.maps.Map.getMap(div);  map.one(plugin.google.maps.event.MAP_READY, function() {console.log('--> map_canvas1 : ready.');});  ");
   }, loadDelay);
 }
 
