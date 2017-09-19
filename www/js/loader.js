48c722ed0a6e97db1fe90318ce554d7791545898
thomazottoni <thomaz.ottoni@gmail.com>
2017-09-18 22:25:20 -0300

    Add files via upload

--- www/js/loader.js
+++ www/js/loader.js
@@ -71,49 +71,49 @@ function executeCode(pTag, loadDelay) {
       });
 
       var isRunning = false;
-  var inputField = mapDiv.getElementsByTagName("input")[0];
-  var button = mapDiv.getElementsByTagName("button")[0];
-  button.addEventListener("click", function() {
-    if (isRunning) {
-      return;
-    }
-    isRunning = true;
+      var inputField = div.getElementsByTagName("input")[0];
+      var button = div.getElementsByTagName("button")[0];
+      button.addEventListener("click", function() {
+        if (isRunning) {
+          return;
+        }
+        isRunning = true;
 
-    var dialogDiv = showVirtualDialog(mapDiv, "Just a moment, please ...");
-    setTimeout(function() {
-      mapDiv.removeChild(dialogDiv);
-    }, 3000);
+        var dialogDiv = showVirtualDialog(div, "Just a moment, please ...");
+        setTimeout(function() {
+          div.removeChild(dialogDiv);
+        }, 3000);
 
-    // Address -> latitude,longitude
-    plugin.google.maps.Geocoder.geocode({
-      "address": inputField.value
-    }, function(results) {
+        // Address -> latitude,longitude
+        plugin.google.maps.Geocoder.geocode({
+          "address": inputField.value
+        }, function(results) {
 
-      if (results.length) {
+          if (results.length) {
 
-        // Add a marker
-        map.addMarker({
-          'position': results[0].position,
-          'title':  JSON.stringify(results[0].position)
-        }, function(marker) {
+            // Add a marker
+            map.addMarker({
+              'position': results[0].position,
+              'title':  JSON.stringify(results[0].position)
+            }, function(marker) {
 
-          // Move to the position
-          map.animateCamera({
-            'target': results[0].position,
-            'zoom': 17
-          }, function() {
-            marker.showInfoWindow();
+              // Move to the position
+              map.animateCamera({
+                'target': results[0].position,
+                'zoom': 17
+              }, function() {
+                marker.showInfoWindow();
+                isRunning = false;
+              });
+
+            });
+
+          } else {
             isRunning = false;
-          });
+          }
 
         });
-
-      } else {
-        isRunning = false;
-      }
-
-    });
-  });
+      });
 
  
     });
