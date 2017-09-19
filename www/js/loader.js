add88a6fbd8be968f3320771e80c5d56385e14ee
thomazottoni <thomaz.ottoni@gmail.com>
2017-09-19 09:21:57 -0300

    Add files via upload

--- www/js/loader.js
+++ www/js/loader.js
@@ -25,96 +25,86 @@ function executeCode(pTag, loadDelay) {
           'icon': "./images/user-location.png",
           'title': msg
 
-        }, function(marker) {
-          marker.showInfoWindow();
-          map.animateCamera({
-            target: location.latLng,
-            zoom: 16
-          }, function() {
+          }, function(marker) {
             marker.showInfoWindow();
+            map.animateCamera({
+              target: location.latLng,
+              zoom: 16
+            }, function() {
+              marker.showInfoWindow();
+            });
           });
+        };
+
+        var onError = function(msg) {
+          alert(JSON.stringify(msg));
+        };
+
+        map.clear();
+        map.getMyLocation(onSuccess, onError);
+
+        var baseArray = new plugin.google.maps.BaseArrayClass([
+          {lat: -20.337862, lng: -40.302598},
+          {lat: -20.336052, lng: -40.309615},
+          {lat: -20.338043, lng: -40.297277},
+          {lat: -20.343194, lng: -40.297749},
+          {lat: 41.79567, lng: 140.75845},
+          {lat: 41.794470000000004, lng: 140.75714000000002},
+          {lat: 41.795010000000005, lng: 140.75611},
+          {lat: 41.79477000000001, lng: 140.75484},
+          {lat: 41.79576, lng: 140.75475},
+          {lat: 41.796150000000004, lng: 140.75364000000002},
+          {lat: 41.79744, lng: 140.75454000000002},
+          {lat: 41.79909000000001, lng: 140.75465}
+        ]);
+
+        baseArray.map(function(element, cb) {
+
+          map.addMarker({
+            "position": element
+          }, cb);
+
+        }, function(markers) {
+
+          // markers[0] ... markers[n]
+
         });
-      };
 
-      var onError = function(msg) {
-        alert(JSON.stringify(msg));
-      };
+        var isRunning = false;
+        var inputField = div.getElementsByTagName("input")[0];
+        var button = div.getElementsByTagName("button")[0];
+        button.addEventListener("click", function() {
+          if (isRunning) {
+            return;
+          }
+          isRunning = true;
 
-      map.clear();
-      map.getMyLocation(onSuccess, onError);
+          var dialogDiv = showVirtualDialog(div, "Um momento, por favor ...");
+          setTimeout(function() {
+            div.removeChild(dialogDiv);
+          }, 3000);
 
-      var baseArray = new plugin.google.maps.BaseArrayClass([
-        {lat: -20.337862, lng: -40.302598},
-        {lat: -20.336052, lng: -40.309615},
-        {lat: -20.338043, lng: -40.297277},
-        {lat: -20.343194, lng: -40.297749},
-        {lat: 41.79567, lng: 140.75845},
-        {lat: 41.794470000000004, lng: 140.75714000000002},
-        {lat: 41.795010000000005, lng: 140.75611},
-        {lat: 41.79477000000001, lng: 140.75484},
-        {lat: 41.79576, lng: 140.75475},
-        {lat: 41.796150000000004, lng: 140.75364000000002},
-        {lat: 41.79744, lng: 140.75454000000002},
-        {lat: 41.79909000000001, lng: 140.75465}
-      ]);
+          // Address -> latitude,longitude
+          plugin.google.maps.Geocoder.geocode({
+            "address": inputField.value
+          }, function(results) {
 
-      baseArray.map(function(element, cb) {
+            if (results.length) {
 
-        map.addMarker({
-          "position": element
-        }, cb);
-
-      }, function(markers) {
-
-        // markers[0] ... markers[n]
-
-      });
-
-      var isRunning = false;
-      var inputField = div.getElementsByTagName("input")[0];
-      var button = div.getElementsByTagName("button")[0];
-      button.addEventListener("click", function() {
-        if (isRunning) {
-          return;
-        }
-        isRunning = true;
-
-        var dialogDiv = showVirtualDialog(div, "Um momento, por favor ...");
-        setTimeout(function() {
-          div.removeChild(dialogDiv);
-        }, 3000);
-
-        // Address -> latitude,longitude
-        plugin.google.maps.Geocoder.geocode({
-          "address": inputField.value
-        }, function(results) {
-
-          if (results.length) {
-
-            // Add a marker
-            map.addMarker({
-              'position': results[0].position,
-              'title':  JSON.stringify(results[0].position)
-            }, function(marker) {
-
-              // Move to the position
+               // Move to the position
               map.animateCamera({
                 'target': results[0].position,
                 'zoom': 17
               }, function() {
-                marker.showInfoWindow();
                 isRunning = false;
               });
 
-            });
-
-          } else {
-            isRunning = false;
-          }
+            } else {
+              isRunning = false;
+            }
+          });
+        });
       });
-
- 
-    });
   }, loadDelay);
 }
 
