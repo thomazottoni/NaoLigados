f3700e6eabf2311c9c9592291cba2fac29b66ec6
thomazottoni <thomaz.ottoni@gmail.com>
2017-09-18 22:08:29 -0300

    Add files via upload

--- www/js/loader.js
+++ www/js/loader.js
@@ -70,6 +70,51 @@ function executeCode(pTag, loadDelay) {
 
       });
 
+      var isRunning = false;
+  var inputField = mapDiv.getElementsByTagName("input")[0];
+  var button = mapDiv.getElementsByTagName("button")[0];
+  button.addEventListener("click", function() {
+    if (isRunning) {
+      return;
+    }
+    isRunning = true;
+
+    var dialogDiv = showVirtualDialog(mapDiv, "Just a moment, please ...");
+    setTimeout(function() {
+      mapDiv.removeChild(dialogDiv);
+    }, 3000);
+
+    // Address -> latitude,longitude
+    plugin.google.maps.Geocoder.geocode({
+      "address": inputField.value
+    }, function(results) {
+
+      if (results.length) {
+
+        // Add a marker
+        map.addMarker({
+          'position': results[0].position,
+          'title':  JSON.stringify(results[0].position)
+        }, function(marker) {
+
+          // Move to the position
+          map.animateCamera({
+            'target': results[0].position,
+            'zoom': 17
+          }, function() {
+            marker.showInfoWindow();
+            isRunning = false;
+          });
+
+        });
+
+      } else {
+        isRunning = false;
+      }
+
+    });
+  });
+
  
     });
   }, loadDelay);
