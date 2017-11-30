831dd573773eee9b073a1c8d253d981935d1d65d
thomazottoni <thomaz.ottoni@gmail.com>
2017-11-30 06:16:10 -0200

    Add files via upload

--- www/js/loader.js
+++ www/js/loader.js
@@ -14,6 +14,7 @@ document.addEventListener("deviceready", function() {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
+    alert(errorCode + " - " + errorMessage);
   });
 });
 
